3472b85bdd80aeed5a950168e2a871aa437a7115
Mahendra Pamidi <mahendrapamidi96@gmail.com>
2017-12-06 05:37:51 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,6 +2,8 @@
 # out the current state.
 def display_board(a)
   puts " #{a[0]} "+"|"+" #{a[1]} "+"|"+" #{a[2]} "
+  puts "-----------"
   puts " #{a[3]} "+"|"+" #{a[4]} "+"|"+" #{a[5]} "
+  puts "-----------"
   puts " #{a[6]} "+"|"+" #{a[7]} "+"|"+" #{a[8]} "
 end
